bf017c50cfb5c56914b19fee61128f8af659ef45
Inokcode <regilearn@gmail.com>
2022-09-12 15:41:14 +0530

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,12 @@
+import "../App.css";
+import { CssBaseline } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+
+const App = () => (
+  <div>
+    <CssBaseline />
+    <main>{/* <Routes><Route path="/" element={}/> </Routes> */}</main>
+    App
+  </div>
+);
+export default App;
