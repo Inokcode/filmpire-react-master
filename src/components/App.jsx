36a936955f76eb09d3c5c85c1a730d156391f7d9
Inokcode <regilearn@gmail.com>
2022-09-14 15:54:19 +0530

    components added

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,23 @@
 import "../App.css";
 import { CssBaseline } from "@mui/material";
 import { Routes, Route } from "react-router-dom";
+import { Actors, MovieInformation, Movies, Navbar, Profile } from "./";
+import { Box } from "@mui/system";
 
 const App = () => (
-  <div>
+  <Box sx={{ display: "flex", height: "100%" }}>
     <CssBaseline />
-    <main>{/* <Routes><Route path="/" element={}/> </Routes> */}</main>
-    App
-  </div>
+    <Navbar />
+
+    <Box sx={{ height: "200px" }}>
+      <Box sx={{ flexGrow: "1", padding: "2em" }} />
+      <Routes>
+        <Route path="/" element={<Movies />} />
+        <Route path="movie/:id" element={<MovieInformation />} />
+        <Route path="actors/:id" element={<Actors />} />
+        <Route path="profile/:id" element={<Profile />} />
+      </Routes>
+    </Box>
+  </Box>
 );
 export default App;
